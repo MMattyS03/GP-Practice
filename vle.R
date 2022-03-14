39edd7f565c87187e3bd77cba31c47a1738beb4a
MMattyS03 <m.southall@newcastle.ac.uk>
2022-03-14 20:49:01 +0000

    Creating a bar plot for code_module against count

--- vle.R
+++ vle.R
@@ -2,3 +2,8 @@
 library(data.table)
 vle=fread('/Users/megansouthall/Documents/Masters/GroupProject/anonymisedData/vle.csv')
 head(vle)
+library(dplyr)
+library(tidyverse)
+
+# Creating a histogram for code_module against count
+ggplot(data=vle)+geom_bar(mapping=aes(x=code_module))
